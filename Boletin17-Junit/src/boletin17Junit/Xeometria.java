8ae5497c6639c6a715483aac9f57425250763552
Colla7o <collazo73@gmail.com>
2017-05-04 10:35:36 +0200

    Segundo Intento

--- Boletin17-Junit/src/boletin17Junit/Xeometria.java
+++ Boletin17-Junit/src/boletin17Junit/Xeometria.java
@@ -1,22 +1,19 @@
 package boletin17Junit;
 
-import java.util.Scanner;
-
 public class Xeometria {
-	public static void main(String[] ar) {
-		int perimetroCadrado, areaCadrado;
-		double teoremaPitagoras;
-		Scanner teclado;
 
-		teclado = new Scanner(System.in);
-		System.out.print("\nDame perimetroCadrado:");
-		perimetroCadrado = teclado.nextInt();
-		System.out.print("Dame areaCadrado:");
-		areaCadrado = teclado.nextInt();
-		System.out.print("teoremapitagoras:");
-		System.out.println((perimetroCadrado * perimetroCadrado) + (areaCadrado * areaCadrado));
-		teclado.close();
-
+	int perimetroCadrado(int lado) {
+		return lado;
 	}
 
+	int areaCadrado(int lado) {
+		return lado;
+	}
+
+	double teoremaPitagoras(int CatetoA, int CatetoB) {
+		return CatetoA;
+		return CatetoB;
+		
+
+	}
 }
