7b2e29b10e05d148a39dae9182ff3c9e43f78123
Colla7o <collazo73@gmail.com>
2017-05-11 10:32:16 +0200

    Intento dni

--- Boletin17-Junit/src/boletin17Junit/DNI.java
+++ Boletin17-Junit/src/boletin17Junit/DNI.java
@@ -1,5 +1,10 @@
 package boletin17Junit;
 
 public class DNI {
-
+	char calculaLetra(int dni) {
+		String juegoCaracteres = "TRWAGMYFPDXBNJZSQVHLCKE";
+		int modulo = dni % 23;
+		char letra = juegoCaracteres.charAt(modulo);
+		return letra;
+	}
 }
