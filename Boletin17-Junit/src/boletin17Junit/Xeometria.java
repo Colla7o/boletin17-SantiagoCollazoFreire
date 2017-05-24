2ae52f5932c576dac48efebb954bd7650c9bfb63
Colla7o <collazo73@gmail.com>
2017-05-24 09:48:38 +0200

    JavaDoc

--- Boletin17-Junit/src/boletin17Junit/Xeometria.java
+++ Boletin17-Junit/src/boletin17Junit/Xeometria.java
@@ -3,6 +3,12 @@ package boletin17Junit;
 import java.lang.Math;
 
 public class Xeometria {
+	/**
+	 * Recibe valor para cada un dos lados e retorna o valor do perimetro total
+	 * 
+	 * @param lado
+	 * @return perimetro
+	 */
 
 	int perimetroCadrado(int lado) {
 		int perimetro;
@@ -11,7 +17,11 @@ public class Xeometria {
 
 		return perimetro;
 	}
-
+/**
+ * Recibe o valor para cada un dos lados, e retorna a area total do cadrado
+ * @param lado
+ * @return area
+ */
 	int areaCadrado(int lado) {
 		int area;
 
@@ -19,6 +29,12 @@ public class Xeometria {
 
 		return area;
 	}
+	/**
+	 * Recibe o valor de cada un dos catetos para calcular mediante a raíz cadrada a hipotenusa
+	 * @param catetoA
+	 * @param catetoB
+	 * @return hipotenusa
+	 */
 
 	double teoremaPitagoras(int catetoA, int catetoB) {
 
@@ -29,6 +45,12 @@ public class Xeometria {
 		return hipotenusa;
 
 	}
+	/**
+	 * Recibe o valor da hipotenusa e dun cateto, para calcular o outro cateto.
+	 * @param hipotenusa
+	 * @param catetoA
+	 * @return catetoB
+	 */
 
 	double teoremaPitagoras1(double hipotenusa, double catetoA) {
 
